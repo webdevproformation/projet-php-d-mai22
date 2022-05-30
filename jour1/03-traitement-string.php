9c6db1413b8c09f6e1194457d82619161ac25a2a
Malik <malik.h@webdevpro.net>
2022-05-30 11:21:37 +0200

    enoncé

--- jour1/03-traitement-string.php
+++ jour1/03-traitement-string.php
@@ -79,4 +79,10 @@ var_dump( htmlspecialchars($virus) );
 var_dump( htmlentities($virus) ); // é à è => &eacute;
 echo htmlspecialchars($virus) ; 
 
-// 
+// cas pratique créer le fichier 04-exo.php 
+
+$texte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu felis euismod, tempus justo et, lacinia lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Pellentesque et libero vel libero hendrerit porttitor. Donec vestibulum turpis sed imperdiet porta. Quisque ac nulla blandit, ornare libero in, vulputate ante. Donec mauris urna, vestibulum ut nisi sed, aliquam pharetra leo. Etiam vel faucibus neque. Suspendisse sodales erat risus. Aenean dignissim finibus sem, elementum porttitor diam convallis sit amet. Cras egestas mi ipsum, sed tempor orci mollis in. Vestibulum eu velit vel diam venenatis rhoncus non et eros. Duis vitae nulla quis urna cursus porta. Vivamus pretium porta mauris. Suspendisse a bibendum velit. ";
+
+// créer une fonction qui permet traitement pour récupérer les 12 mots de la chaine de caractères et ajouter à la fin ...
+
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu felis euismod, ...
