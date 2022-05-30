ee0a5b3e96151cf4e38a4fbb4f41603dbf84b35c
Malik <malik.h@webdevpro.net>
2022-05-30 14:14:21 +0200

    cas pratique

--- jour1/07-array-suite.php
+++ jour1/07-array-suite.php
@@ -52,3 +52,20 @@ var_dump($page , $filtre);
 $sujets = ["PHP" , "JS" , "HTML" , "CSS" , "Cobol"];
 var_dump(current($sujets)); // 1er
 var_dump(end($sujets)); // last
+
+// $_SERVER ??? 
+var_dump($_SERVER); // gros tableau associatif // comme $_GET et $_POST , $_SERVER est une variable super globale de PHP => donne des infos sur l'environnement d'exécution de votre script php
+
+
+// cas pratique un nouveau fichier 08-exo.php 
+
+// utiliser la variable super globale $_SERVER 
+
+// 1 est ce que ce tableau associatif dispose de la key => HTTP_HOST ?
+// 2 à partir de cette variable reconstituer l''url du fichier en cours d'exécution 
+// http://localhost/projet-php/jour1/07-array-suite.php
+// 3 combien d'élément contient cette super globale
+// 4 pouvez vous me récupérer la version de PHP qui est actuellement utilisé dans votre système ??
+// récupérer le contenu de l'item PATH et transformer en tableau indexé via le symbole ; 
+
+// router en PHP 
