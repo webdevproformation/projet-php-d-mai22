8807ba5de5713e8ac93bddcfba8dbc8b7b28ae20
Malik <malik.h@webdevpro.net>
2022-05-31 14:16:36 +0200

    enoncé

---
+++ jour2/08-session-suite.php
@@ -0,0 +1,26 @@
+<?php // http://localhost/projet-php/jour2/08-session-suite.php
+session_start();
+// stocker des informations tout au long de la navigation de l'utilisateur
+// dans votre site 
+
+// permet de conserver des informations pendant toute la session de l'utilisateur
+
+// langage => 
+// si la personne s'est bien connecté pour accès au back office
+// panier d'achat 
+
+// ne pas mettre de saut de ligne / espace avant la balise ouvrante php
+// "Headers already sent"
+
+var_dump($_SESSION); 
+
+// créer le fichier 09-exo.php 
+// une page php dans laquelle 
+// lien => fr 
+// lien => en
+
+// <p>si la session contient fr ou rien => 
+//bonjour
+// si la session contient en 
+// hello
+//</p>
