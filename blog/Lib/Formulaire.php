dabf3c5d26fd7e84b01d9c0f344a2b3cb1d2d891
Malik <malik.h@webdevpro.net>
2022-06-01 10:32:29 +0200

    formulaire

---
+++ blog/Lib/Formulaire.php
@@ -0,0 +1,72 @@
+<?php 
+
+
+/**
+ * class utilitaire permettant de générer des champs de formulaire dans les pages html
+ */
+class Formulaire{
+
+    /**
+     * méthode privée permettant de créer n'importe quelle input 
+     *
+     * @param string $nom
+     * @param string $label
+     * @param string $type
+     * @return string
+     */
+    private function input(string $nom , string $label , string $type ) : string {
+        return "<div class=\"mb-3\">
+            <label for=\"$nom\">$label</label>
+            <input type=\"$type\" id=\"$nom\" class=\"form-control\" name=\"$nom\">
+        </div>";
+    }
+
+    /**
+     * générer un champ de formulaire input type text
+     *
+     * @param string $nom
+     * @param string $label
+     * @return string
+     */
+    public function inputText(string $nom , string $label) :string{
+        return $this->input($nom , $label , "text");
+    }
+    
+    /**
+     * générer un champ de formulaire input type email
+     *
+     * @param string $nom
+     * @param string $label
+     * @return string
+     */
+    public function inputEmail(string $nom , string $label) :string{
+        return $this->input($nom , $label , "email");
+    }
+
+    /**
+     * générer un champ de formulaire textarea
+     *
+     * @param string $nom
+     * @param string $label
+     * @return string
+     */
+    public function textarea(string $nom , string $label) :string{
+        return "<div class=\"mb-3\">
+            <label for=\"$nom\">$label</label>
+            <textarea name=\"$nom\" id=\"$nom\" cols=\"30\" rows=\"10\" class=\"form-control\"></textarea>
+        </div>";
+    }
+
+    /**
+     * générer une bouton de soumission de formulaire 
+     *
+     * @param string $style
+     * @return string
+     */
+    public function submit(string $style = "btn-primary") :string{
+        return "<div class=\"mb-3\">
+            <input type=\"submit\" class=\"btn $style\">
+        </div>";
+    }
+
+}
