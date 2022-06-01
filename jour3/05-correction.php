a404b6e1ba03bb498329864a46a909a46d5db1eb
Malik <malik.h@webdevpro.net>
2022-06-01 12:38:34 +0200

    correction étape 1

---
+++ jour3/05-correction.php
@@ -0,0 +1,52 @@
+<!-- http://localhost/projet-php/jour3/05-correction.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>correction</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header class="bg-warning">
+        <nav class="container navbar navbar-expand navbar-light px-3">
+            <a href="" class="navbar-brand">TP PHP</a>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">Accueil</a></li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a href="" class="nav-link">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container">
+        <h1>Bienvenu</h1>
+        <section class="row">
+            <!-- si j'ai du code qui se répète => emballer avec une boucle for -->
+            <?php for($i = 0 ; $i < 10 ; $i++) : ?>
+            <article class="col-4 mb-3">
+                <div class="card">
+                    <h2 class="card-header">titre</h2>
+                    <img src="https://via.placeholder.com/300x200" alt="">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, blanditiis.
+                    </div>
+                    <footer class="card-footer">
+                        <button class="btn btn-primary btn-sm">lire la suite</button>
+                    </footer>
+                </div>
+            </article>
+            <?php endfor ?>
+        </section>  
+    </main>
+</body>
+</html>
+
+<!--
+    d'abord => html / css (bootstrap)
+    deux zones entête / corps de page 
+             <header>    <main>
+    entête fond orange => bg-warning => background attention 
+    https://getbootstrap.com/docs/5.2/components/card/
+-->
